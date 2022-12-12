d91a8271d68e7a82fe622a0b7236de542b10700e
ThomasDufour19 <dufourthomas159@gmail.com>
2022-12-12 11:39:30 +0100

    update name

--- jon/herds.php
+++ jon/herds.php
@@ -1,10 +1,10 @@
-<php @extends('layout.app') ?></php>
+<?php include_once('../layout/app.php'); ?>
       <div class="about">
          <div class="container">
             <div class="row">
                <div class="col-md-12">
                   <div class="titlepage">
-                     <h2>People Says About Farm</h2>
+                     <h2>Herds</h2>
                      
                   </div>
                </div>
@@ -26,7 +26,6 @@
                                        <div class="test_box">
                                           <i><img src="images/tes1.png" alt="#"/></i>
                                           <h4>jhone Du</h4>
-                                          <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,</p>
                                        </div>
                                     </div>
                                  </div>
@@ -41,7 +40,6 @@
                                        <div class="test_box">
                                           <i><img src="images/tes1.png" alt="#"/></i>
                                           <h4>jhone Du</h4>
-                                          <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,</p>
                                        </div>
                                     </div>
                                  </div>
@@ -56,7 +54,6 @@
                                        <div class="test_box">
                                           <i><img src="images/tes1.png" alt="#"/></i>
                                           <h4>jhone Du</h4>
-                                          <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,</p>
                                        </div>
                                     </div>
                                  </div>
@@ -77,98 +74,9 @@
             </div>
          </div>
       </div>
-      <!-- end about -->
       </div>
-      <!--  footer -->
-      <footer id="contact">
-         <div class="footer">
-            <div class="container">
-               <div class="row">
-                  <div class="col-md-4">
-                     <div class="titlepage">
-                        <h2>Contact Us</h2>
-                     </div>
-                  </div>
-                  <div class="col-md-12">
-                     <form id="request" class="main_form">
-                        <div class="row">
-                           <div class="col-md-3 ">
-                              <input class="contactus" placeholder="Full Name" type="type" name="Full Name"> 
-                           </div>
-                           <div class="col-md-3">
-                              <input class="contactus" placeholder="Email" type="type" name="Email"> 
-                           </div>
-                           <div class="col-md-3">
-                              <input class="contactus" placeholder="Phone Number" type="type" name="Phone Number">                          
-                           </div>
-                           <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12">
-                              <ul class="social_icon">
-                                 <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                                 <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                                 <li><a href="#"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
-                                 <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-                              </ul>
-                           </div>
-                           <div class="col-md-8">
-                              <textarea class="contactus1" placeholder="Message" type="type" Message="Name">Message </textarea>
-                           </div>
-                           <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
-                              <button class="send_btn">Send</button>
-                           </div>
-                           
-                        </div>
-                     </form>
-                  </div>
-                  
-                   <div class="col-md-3 border_right">
-                     <ul class="location_icon">
-                        <li><a href="#"><i class="fa fa-map-marker" aria-hidden="true"></i></a> Locatins</li>
-                        <li><a href="#"><i class="fa fa-volume-control-phone" aria-hidden="true"></i></a> +71 9087654321</li>
-                        <li><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i></a>demo@gmail.com</li>
-                     </ul>
-                  </div>
-                  <div class="col-md-3 border_right">
-                     <h3>Useful Link</h3>
-                     <ul class="link">
-                        <li><a href="#">humour, or </a></li>
-                        <li><a href="#">randomised words </a> </li>
-                        <li><a href="#">which don't look </a></li>
-                        <li><a href="#">even slightly  </a> </li>
-                        <li><a href="#">believable. If   </a></li>
-                     </ul>
-                  </div>
-                  <div class="col-md-3 border_right">
-                     <h3>Menus</h3>
-                     <ul class="link">
-<li><a href="index.html">Home</a></li>                             
-<li><a href="herds.html">About</a></li>                                                     
-<li><a href="cereals.html">Products</a></li>                             
-<li><a href="orders.html">Blog</a></li>
-<li><a href="contact.html">Contact</a></li>
-                     </ul>
-                  </div>
-                  <div class="col-md-3">
-                     <form class="bottom_form">
-                        <h3>Newsletter</h3>
-                        <input class="enter" placeholder="Enter your email" type="text" name="Enter your email">
-                        <button class="sub_btn">subscribe</button>
-                     </form>
-                  </div>
-               </div>
-            </div>
-            <div class="copyright">
-               <div class="container">
-                  <div class="row">
-                     <div class="col-md-12">
-                        <p>Copyright 2019 All Right Reserved By <a href="https://html.design/"> Free  html Templates</a></p>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </footer>
-      <!-- end footer -->
-      <!-- Javascript files-->
+      <?php include_once('../layout/footer.php'); ?>
+
       <script src="js/jquery.min.js"></script>
       <script src="js/popper.min.js"></script>
       <script src="js/bootstrap.bundle.min.js"></script>
@@ -176,5 +84,3 @@
       <!-- sidebar -->
       <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
       <script src="js/custom.js"></script>
-   </body>
-</html>
